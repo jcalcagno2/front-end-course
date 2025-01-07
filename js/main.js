bbbd5ea8c4d1616b929e9a82cbb2f1c2e63e1636
Joseph Calcagno <j_calca2@uncg.edu>
2025-01-06 19:35:59 -0500

    Javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+var isPremiumUser = false;
 
+if (isPremiumUser) {
+    alert("Thanks for being a loyal customer")
+}
+else {
+    alert("False")
+} 
 
-var myAge = 28;
+var myAge = 3;
 
+if (myAge < 1) {
+    console.log("Youre are a boby")
 
-console.log("MY age is" + myAge);
-
-myAge = 29;
-
-console.log("My age next yeat" + myAge)
+} else if (myAge < 3) {
+    console.log("Older")
+}
+else if(myAge < 10) {
+    console.log("You are big kid")
+} else {
+    console.log("You must be too old.")
+}
