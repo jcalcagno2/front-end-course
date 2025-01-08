a225c7d40efa412af4d00b0f788cebb08142ead8
Joseph Calcagno <j_calca2@uncg.edu>
2025-01-07 20:44:53 -0500

    Javascript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-function buttonClicked() {
-    console.log("Button Clicked");
-    btn.removeEventListener("click", buttonClicked);
-    document.getElementById("text").innerHTML = "Dont do it"
-    console.log(document.getElementById("text").innerHTML);
-}
-
 
 var btn = document.getElementById("go-button");
 
+function buttonClicked() {
+    console.log("Button Clicked");
+
+    var customText= document.getElementsByClassName("my-input");
+    var results = document.getElementById("text");
+    var textArea = document.getElementsByClassName("my-textarea");
+
+    
+    results.innerHTML = "Hello" + customText[0].value + "<br />";
+
+
+
+    results.innerHTML += "Message: " + textArea[0].value;
+
+}
+
+
 btn.addEventListener("click", buttonClicked);
+
+
+
