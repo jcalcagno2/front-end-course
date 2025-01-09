229f903fdc0c191b1048afda37996f063a52e696
Joseph Calcagno <j_calca2@uncg.edu>
2025-01-09 17:05:07 -0500

    Moving on

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
+class Animal {
 
-var btn = document.getElementById("go-button");
-
-function buttonClicked() {
-    console.log("Button Clicked");
-
-    var customText= document.getElementsByClassName("my-input");
-    var results = document.getElementById("text");
-    var textArea = document.getElementsByClassName("my-textarea");
-
-    
-    results.innerHTML = "Hello" + customText[0].value + "<br />";
-
-
-
-    results.innerHTML += "Message: " + textArea[0].value;
+    constructor(name, height, weight) {
+        console.log("Created", name, height, weight);
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
 
 }
 
+    nameLength(name) {
+        return this.name.length;
+    }
 
-btn.addEventListener("click", buttonClicked);
+}
+
+class Dog extends Animal {
+    
+    constructor(name, height, weight, barkVolume, leshColor) {
+        super(name, height, weight);
+        
+        this.barkVolume = barkVolume;
+        this.leshColor = leshColor;
+
+    }
+
+    bark() {
+        if(this.barkVolume > 50) {
+            console.log(this.name, "Barks Loudly");
+
+        } else {
+            console.log(this.name, "Batks timidly");
+        }
+    }
+}
+
+class Fish extends Animal {
+    constructor(name, height, weight, swimSpeed) {
+        super(name, height, weight);
+        this.swimSpeed = swimSpeed;
+        
+    }
+    swim() {
+        
+    }
+}
 
 
+var king = new Dog("King", 45, 92, 72, "red");
 
+king.bark();
