1369ec0fdfca0755937a6711ffcfad175db88c33
Joseph Calcagno <j_calca2@uncg.edu>
2025-01-10 11:40:20 -0500

    Jquery

--- js/main.js
+++ js/main.js
@@ -1,51 +1,12 @@
-class Animal {
+$(document).ready(function() {
 
-    constructor(name, height, weight) {
-        console.log("Created", name, height, weight);
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
+    alert("Loaded");
 
-}
+    document.getElementById('text').innerHTML = "This is my text";
 
-    nameLength(name) {
-        return this.name.length;
-    }
+    $('#text').html("This is now my text");
 
-}
-
-class Dog extends Animal {
-    
-    constructor(name, height, weight, barkVolume, leshColor) {
-        super(name, height, weight);
-        
-        this.barkVolume = barkVolume;
-        this.leshColor = leshColor;
-
-    }
-
-    bark() {
-        if(this.barkVolume > 50) {
-            console.log(this.name, "Barks Loudly");
-
-        } else {
-            console.log(this.name, "Batks timidly");
-        }
-    }
-}
-
-class Fish extends Animal {
-    constructor(name, height, weight, swimSpeed) {
-        super(name, height, weight);
-        this.swimSpeed = swimSpeed;
-        
-    }
-    swim() {
-        
-    }
-}
+    $('.my-input').val("New Input Val");
 
 
-var king = new Dog("King", 45, 92, 72, "red");
-
-king.bark();
+});
